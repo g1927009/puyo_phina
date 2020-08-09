3ca8d4096c94cb84029b1090c7afa76061a703e2
g1927009 <55940009+g1927009@users.noreply.github.com>
2020-08-09 16:19:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -396,7 +396,7 @@ class Player{
     }
     if(!isBlocked) {
       // 下にブロックがないなら自由落下してよい。プレイヤー操作中の自由落下処理をする
-      this.puyoStatus.top += CONFIG_PLAYER_FALLING_SPEED + (CONFIG_PLAYER_PLUS_SPEED * (this.score/1000));
+      this.puyoStatus.top += CONFIG_PLAYER_FALLING_SPEED;
       if(isDownPressed) {
         // 下キーが押されているならもっと加速する
         this.puyoStatus.top += CONFIG_PLAYER_DOWN_SPEED + (CONFIG_PLAYER_PLUS_SPEED * (this.score/1000));
